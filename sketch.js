00e8fe0b7ef2d39e64531bb711290bf4d021b326
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-08 15:46:54 -0500

    Class Work

--- sketch.js
+++ sketch.js
@@ -1,10 +1,22 @@
 /// <reference path="./libraries/p5.d.ts" />
 
 function setup() {
-  createCanvas(400, 400);
-  
+  createCanvas(800, 800);
+  background(0);
 }
 
+x = 200;
+y = 200;
+interp = 0.15;
+
 function draw() {
-  background(220);
+  noStroke();
+  fill(mouseX / 400 * 255,mouseY / 400 * 255,(mouseX + mouseY) / 800 * 255,50);
+  interpolateCoordinates();
+  ellipse(x, y, 75, 75);
+}
+
+function interpolateCoordinates() {
+  x = x + (mouseX - x) * interp;
+  y = y + (mouseY - y) * interp;
 }
