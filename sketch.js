4953f3377fbac2b91b529fde276d2f0bf654496d
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-10 14:47:05 -0500

    fulfilled exercise requirements

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(400, 400);
   background(220);
 }
 
 function draw() {
+  fill(mouseX/400 * 255,(1 - mouseY / 400) * 255,100)
+  rect(200,200,100,100);
+  fill(200,244,40)
+  rect(100,frameCount,50,50)
+}
 
+function mouseClicked() {
+  ellipseMode(CENTER)
+  fill(50,200,200)
+  ellipse(mouseX,mouseY,30,30)
 }
