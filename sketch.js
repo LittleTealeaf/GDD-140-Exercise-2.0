1f3fe15a0335aa303772115fbc22eaf7c64ca384
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-10 14:41:17 -0500

    reset sketch

--- sketch.js
+++ sketch.js
@@ -1,22 +1,10 @@
-/// <reference path="./libraries/p5.d.ts" />
+/// <reference path="./libraries/p5.global-mode.d.ts" />
 
 function setup() {
   createCanvas(800, 800);
   background(0);
 }
 
-x = 200;
-y = 200;
-interp = 0.15;
-
 function draw() {
-  noStroke();
-  fill(mouseX / 400 * 255,mouseY / 400 * 255,(mouseX + mouseY) / 800 * 255,50);
-  interpolateCoordinates();
-  ellipse(x, y, 75, 75);
-}
 
-function interpolateCoordinates() {
-  x = x + (mouseX - x) * interp;
-  y = y + (mouseY - y) * interp;
 }
