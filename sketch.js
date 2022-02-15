5b5b34be053d8bb285329d216401b3cf908d0caa
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-14 19:33:52 -0500

    Added comments

--- sketch.js
+++ sketch.js
@@ -3,17 +3,21 @@
 function setup() {
   createCanvas(400, 400);
   background(220);
+  ellipseMode(CENTER);
 }
 
 function draw() {
+  //Draw the square
   fill(mouseX/400 * 255,(1 - mouseY / 400) * 255,100)
   rect(200,200,100,100);
+
+  //Draw the moving rectangle
   fill(200,244,40)
   rect(100,frameCount,50,50)
 }
 
 function mouseClicked() {
-  ellipseMode(CENTER)
+  //Draw the ellipse at the mouse location
   fill(50,200,200)
   ellipse(mouseX,mouseY,30,30)
 }
